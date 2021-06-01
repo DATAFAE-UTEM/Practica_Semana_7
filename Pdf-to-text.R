ae1abd66ea9af210c9ffedd992160404b104ad3f
MartinAguilarR <martin.aguilarr@utem.cl>
2021-06-01 17:19:38 +0200

    Actualización modelo

--- Pdf-to-text.R
+++ Pdf-to-text.R
@@ -5,111 +5,48 @@ rm(list=ls(all=T))
 options(stringsAsFactors = F)
 
 # install packages
-install.packages("stringr")
-install.packages("httr")
-install.packages("jsonlite")
-
-# activate packages
-library(pdftools)
-library(dplyr)
-library(stringr)
-
-# funcion para convertir muchos documentos PDF en txt
-convertpdf2txt <- function(dirpath){
-  files <- list.files(dirpath, full.names = T)
-  x <- sapply(files, function(x){
-    x <- pdftools::pdf_text(x) %>%
-      paste(sep = " ") %>%
-      stringr::str_replace_all(fixed("\n"), " ") %>%
-      stringr::str_replace_all(fixed("\r"), " ") %>%
-      stringr::str_replace_all(fixed("\t"), " ") %>%
-      stringr::str_replace_all(fixed("\""), " ") %>%
-      paste(sep = " ", collapse = " ") %>%
-      stringr::str_squish() %>%
-      stringr::str_replace_all("- ", "")
-    return(x)
-  })
-}
-
-# aplicamos la funcion
-txts <- convertpdf2txt("C:\\Users\\panch\\Desktop\\python\\PDFs")
-
-
-# inspect the structure of the txts element
-str(txts)
-
-
-# add names to txt files
-names(txts) <- paste("text", 1:length(txts), sep = "")
-
-# save result to disc
-lapply(seq_along(txts), function(i)writeLines(text = unlist(txts[i]),
-                                              con = paste("C:\\Users\\panch\\Desktop\\python\\PDF_txt/", names(txts)[i],".txt", sep = "")))
-
-## funciona bien pero para PDFs que son imagenes no
-
-#===========================================================================================#
-
-#===========================================================================================#
-
-#  con esto puedo sacar el texto de un PDF image
-
 install.packages("tesseract")
+install.packages("pdftools")
+install.packages("tidyverse")
+install.packages("wordcloud2")
+install.packages("tm")
 
+
+# Activate packages
 library(tesseract)
+library(pdftools)
+library(wordcloud2)
+library(tm)
+library(tidyverse)
 
-pngfile <- pdftools::pdf_convert("C:\\Users\\marti\\OneDrive\\Desktop\\Datas\\PDf - Modelo extracción Py\\Archivo - 1.pdf", dpi = 600)
-text <- tesseract::ocr(pngfile)
-return(text)
+#===========================================================================================#
+                                  # EXTRACCIÓN DE TEXTO #
+#===========================================================================================#
 
+# MODELO PARA DOCUMENTOS FORMATO PDF #
 
+# Creación de Data y definición de url
+Data_text = data.frame()
+my_dir <- ("Directorio de los archivos .PDF")
 
-library("wordcloud2")
-library("tm")
-
-# ruta donde tengo 100 pdf
-my_dir = "C:\\Users\\marti\\OneDrive\\Desktop\\Datas\\PDf - Modelo extracción Py"
-
-# se pasan los pdf a una lista
+# Extracción del texto
 files <- list.files(path = my_dir, pattern = "pdf$")
-files
-
-# se crea un corpus
 setwd(my_dir)
-
-Data_text = Data.frame()
 
 for (file in files){
 
-  corp <- Corpus(URISource(file, encoding = "latin1"),
-                 readerControl = list(reader = readPDF, language = "es-419"))
+  Archivo = file
 
-  pngfile <- pdftools::pdf_convert(file, dpi = 600)
-  text <- tesseract::ocr(pngfile)
-  cat(text)
+  corp = Corpus(URISource(file, encoding = "latin1"),
+                   readerControl = list(reader = readPDF, language = "es-419"))
 
-  Data_text <- add_row()
-  }
+  pngfile = pdftools::pdf_convert(file, dpi = 600)
+  Texto = tesseract::ocr(pngfile)
+  cat(Texto)
 
+  Fecha = corp[[ file ]][["meta"]][["datetimestamp"]]
 
-# guardo los nobres de los pdf en un vector
-argumento <- names(corp)
-argumento
-
-
-# se hace una funcion que contenga la ruta de totos los PDFs_image
-
-directorios <- data.frame()
-
-for(nombre_archivo in argumento){
-  directorio <- data.frame(
-    ruta = paste("C:\\Users\\marti\\OneDrive\\Desktop\\Datas\\PDf - Modelo extracción Py", nombre_archivo, sep = "")
-  )
-  directorios <- rbind(directorios, directorio)
+  Data_text <- rbind(Data_text, data.frame(Archivo, Fecha, Texto))
 }
-
-# funcion para hacer pasar las rutas por el extractor de texto
-rutas <- directorios$ruta
-
 
 
