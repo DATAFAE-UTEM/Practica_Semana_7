2a99da6d3c5cb85762aae10311b330725967a570
MartinAguilarR <martin.aguilarr@utem.cl>
2021-06-02 15:48:32 +0200

    Update Pdf-to-text.R

--- Pdf-to-text.R
+++ Pdf-to-text.R
@@ -42,16 +42,37 @@ for (file in files){
   corp = Corpus(URISource(file, encoding = "latin1"),
                 readerControl = list(reader = readPDF, language = "es-419"))
 
+  # Extracción de fecha
+  Fecha = corp[[ file ]][["meta"]][["datetimestamp"]]
+
   # Transformación del texto
   pngfile = pdftools::pdf_convert(file, dpi = 600)
   Texto = tesseract::ocr(pngfile)
   cat(Texto)
 
-  # Extracción de fecha
-  Fecha = corp[[ file ]][["meta"]][["datetimestamp"]]
+  # Transformación lista a párrafo
+  Full_Texto <- ""
+
+  for (i in Texto) {
+    Full_Texto <- paste(Full_Texto, i)
+  }
+
+  Texto <- Full_Texto
+  rm(Full_Texto)
+
+  # Unión y limpieza
+  Texto_limpio = Texto %>% paste(sep = " ") %>%
+                      stringr::str_replace_all(fixed("\n"), " ") %>%
+                      stringr::str_replace_all(fixed("\r"), " ") %>%
+                      stringr::str_replace_all(fixed("\t"), " ") %>%
+                      stringr::str_replace_all(fixed("\""), " ") %>%
+                      paste(sep = " ", collapse = " ")
+  cat(Texto_limpio)
 
   # Construccion Dataset
-  Data_text <- rbind(Data_text, data.frame(Archivo, Fecha, Texto))
+  Data_text <- rbind(Data_text, data.frame(Archivo, Fecha, Texto, Texto_limpio))
 }
+
+write.csv2(Data_text, "C:\\Users\\marti\\OneDrive\\Desktop\\Datas\\Data Profe Diana\\Data_text.csv" )
 
 
